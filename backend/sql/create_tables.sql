94cb7729c1e8b9bd6dfe750c0072896f27a23888
nx-xn2002 <1870230468@qq.com>
2025-01-12 16:31:48 +0800

    🗃️ Update user table

--- backend/sql/create_tables.sql
+++ backend/sql/create_tables.sql
@@ -2,21 +2,19 @@
 create database if not exists echo;
 use echo;
 
+# 用户信息表
 create table if not exists user
 (
-    id           bigint auto_increment
+    id          bigint auto_increment
         primary key,
-    userAccount  varchar(256)                       null comment '用户账号',
-    userName     varchar(256)                       null comment '用户昵称',
-    avatarUrl    varchar(1024)                      null comment '用户头像',
-    gender       tinyint                            null comment '性别',
-    userPassword varchar(512)                       not null comment '密码',
-    phone        varchar(128)                       null comment '电话',
-    email        varchar(512)                       null comment '邮箱',
-    userRole     int      default 1                 null comment '用户角色 0 - 管理员, 1 - 学生, 2 - 老师',
-    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    updateTime   datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    deletedFlag  tinyint  default 0                 not null comment '删除状态 默认0'
+    username    varchar(256)                       not null comment '用户昵称',
+    password    varchar(512)                       not null comment '密码',
+    phone       varchar(128)                       null comment '电话',
+    email       varchar(512)                       null comment '邮箱',
+    role        int      default 1                 null comment '用户角色 0 - 管理员, 1 - 学生, 2 - 老师',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_deleted  tinyint  default 0                 not null comment '删除状态 默认0'
 ) comment '用户表';
 
 create table if not exists question_bank
